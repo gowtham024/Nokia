f478f214fe27ed384223b7ce7b9252240033fb8a
gowtham024 <gowtham.balan24@gmail.com>
2021-05-30 21:12:49 +0530

    mano

---
+++ Employee.java
@@ -0,0 +1,14 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Employee {
+public static void main(String[] args) {
+	Set<Integer> profile=new LinkedHashSet<>();
+	profile.add(10);
+	profile.add(30);
+	profile.add(40);
+	System.out.println(profile);
+	
+}
+	
+
